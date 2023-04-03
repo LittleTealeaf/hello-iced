e52897af0badf4941b0f6b94a4144524c6ea0bdb
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-03 16:21:36 -0400

    iced!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    widget::{button, column, text},
+    Alignment, Sandbox, Settings,
+};
+
+pub fn main() -> iced::Result {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("COUNTER")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        column![
+            button("Increment").on_press(Message::IncrementPressed),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::DecrementPressed),
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
 }
